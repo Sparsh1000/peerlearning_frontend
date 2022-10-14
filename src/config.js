2e37708997fbf0c86aa75d81f0ff70a1afc5bc8c
Sparsh Agrawal <93774571+Sparsh1000@users.noreply.github.com>
2022-10-14 16:21:17 +0530

    first

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// const G_API = "https://classroom.googleapis.com/v1";
+// const API = "https://radiant-fortress-88014.herokuapp.com";
+// module.exports = { G_API, API };
+
+const G_API = "https://classroom.googleapis.com/v1";
+const API = "https://peer-learning-app-backend.onrender.com";
+//const API = "https://peerlearning-backend.herokuapp.com";
+module.exports = { G_API, API };
